316635282ac73e4805015a21c27ee53b1370ab0c
avouacr <avouacr@pm.me>
2022-09-17 09:30:38 +0000

    structure le porjet

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 
-from functions import (
-    import_clean_data,
-    feature_engineering,
-    label_encode_variable,
-    make_val_split,
-    evaluate_rdmf
-)
+from src.data.import_data import import_clean_data
+from src.data.train_test_split import make_val_split
+from src.features.build_features import feature_engineering, label_encode_variable
+from src.models.train_evaluate import evaluate_rdmf
+
 
 if __name__ == "__main__":
-    TrainingData, TestData = import_clean_data()
+    training_data, test_data = import_clean_data("data/raw")
 
-    meanAge = round(TrainingData['Age'].mean())
-    TrainingData = feature_engineering(TrainingData, meanAge)
-    TestData = feature_engineering(TestData, meanAge)
+    mean_age = round(training_data['Age'].mean())
+    training_data = feature_engineering(training_data, mean_age)
+    test_data = feature_engineering(test_data, mean_age)
 
-    TrainingData = label_encode_variable(TrainingData, "Sex")
-    TrainingData = label_encode_variable(TrainingData, "Title")
-    TrainingData = label_encode_variable(TrainingData, "Embarked")
+    training_data = label_encode_variable(training_data, "Sex")
+    training_data = label_encode_variable(training_data, "Title")
+    training_data = label_encode_variable(training_data, "Embarked")
 
-    X_train, X_test, y_train, y_test = make_val_split(TrainingData)
+    X_train, X_test, y_train, y_test = make_val_split(training_data)
 
     evaluate_rdmf(X_train, X_test, y_train, y_test, n_estimators=20)
